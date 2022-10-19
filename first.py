7c30cc5454ed23b1365a4a1801a85cd933329e43
alexismenanieves <alexismenanieves@hotmail.com>
2022-10-19 13:33:42 -0500

    First run

--- first.py
+++ first.py
@@ -1,14 +1,17 @@
 from shiny import ui, render, App
 
+# Interface
 app_ui = ui.page_fluid(
-    ui.input_slider('slider', min=0, max=10, value=5),
+    ui.input_slider('slider', 'choose', min=0, max=10, value=5),
     ui.output_text_verbatim('txt')
 )
 
+# Server logic
 def server(input, output, session):
     @output
     @render.text
     def txt():
         return f'Double is {input.slider()*2}'
 
+# Application
 app = App(app_ui, server)

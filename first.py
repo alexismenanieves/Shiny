19e9f6311f52dd488327b16e38f45fbdb56d9d16
alexismenanieves <alexismenanieves@hotmail.com>
2022-10-19 13:35:54 -0500

    Added comments template

--- first.py
+++ first.py
@@ -1,17 +1,18 @@
+# Step 0. Load libraries and custom modules
 from shiny import ui, render, App
 
-# Interface
+# Step 1. Design interface
 app_ui = ui.page_fluid(
     ui.input_slider('slider', 'choose', min=0, max=10, value=5),
     ui.output_text_verbatim('txt')
 )
 
-# Server logic
+# Step 2. Design server logic
 def server(input, output, session):
     @output
     @render.text
     def txt():
         return f'Double is {input.slider()*2}'
 
-# Application
+# Step 3. Create application
 app = App(app_ui, server)
